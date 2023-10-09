b268e2fae49962d4b6aadea156788d50587c9df5
Sadeesha-Sath <sadeerc@gmail.com>
2023-10-10 01:19:04 +0530

    Make Queries Escaped

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,9 +1,10 @@
 const { query, escapedQuery } = require("../services/db.service.js");
 
 const findOne = async (id) => {
-  const result = await query(
-    `SELECT e.EmployeeID, u.Name, e.BranchID, e.Position, u.Email, u.UserID, u.Username from Employee e left join User u on e.UserID=u.UserID where e.EmployeeID=${id}`
-  );
+  const result = await escapedQuery({
+    sql: `SELECT e.EmployeeID, u.Name, e.BranchID, e.Position, u.Email, u.UserID, u.Username from Employee e left join User u on e.UserID=u.UserID where e.EmployeeID=?`,
+    values: [id],
+  });
   console.log(result[0]);
   return result[0];
 };
@@ -17,21 +18,22 @@ const findAll = async () => {
 };
 
 const findUserIDfromEmployeeID = async (employeeID) => {
-  const result = await query(
-    `SELECT UserID from Employee where EmployeeID=${employeeID}`
-  );
+  const result = await escapedQuery({
+    sql: `SELECT UserID from Employee where EmployeeID=?`,
+    values: [employeeID],
+  });
   console.log(result[0]);
   return result[0];
 };
 
 const findBranchIDfromEmployeeID = async (employeeID) => {
-  const result = await query(
-    `SELECT BranchID from Employee where EmployeeID=${employeeID}`
-  );
+  const result = await escapedQuery({
+    sql: `SELECT BranchID from Employee where EmployeeID=?`,
+    values: [employeeID],
+  });
   console.log(result[0]);
   return result[0];
 };
-
 
 module.exports = {
   findOne,
