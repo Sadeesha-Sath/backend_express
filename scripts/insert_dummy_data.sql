6d73c8fb85da3e00e51eb861baf6782c68495870
Sadeesha-Sath <sadeerc@gmail.com>
2023-10-18 19:43:53 +0530

    Some changes to SQL

--- scripts/insert_dummy_data.sql
+++ scripts/insert_dummy_data.sql
@@ -1,25 +1,29 @@
 -- Insert data into Branch table
 INSERT INTO Branch (BranchID, BranchName, Address, Phone, Email) VALUES 
-('B001', 'Main Branch', '123 Main St, Cityville, XYZ', '123-456-7890', 'mainbranch@example.com'),
-('B002', 'Downtown Branch', '456 Elm St, Cityville, XYZ', '987-654-3210', 'downtownbranch@example.com');
+(NULL, 'Main Branch', '123 Main St, Cityville, XYZ', '123-456-7890', 'mainbranch@example.com'),
+(NULL, 'Downtown Branch', '456 Elm St, Cityville, XYZ', '987-654-3210', 'downtownbranch@example.com');
+-- Query Sep
 
 -- Insert data into User table
 INSERT INTO User (UserID, Name, Role, Username, Email, Password) VALUES 
-('U001', 'Admin User', 'admin', 'admin', 'admin@example.com', 'admin_password'),
-('U002', 'Branch Manager', 'b_manager', 'manager_user', 'manager@example.com', 'manager_password'),
-('U003', 'Employee User', 'employee', 'employee_user', 'employee@example.com', 'employee_password'),
-('U004', 'Customer User 1', 'customer', 'customer_user_1', 'customer1@example.com', 'customer1_password'),
-('U005', 'Customer User 2', 'customer', 'customer_user_2', 'customer2@example.com', 'customer2_password');
+(NULL, 'Admin User', 'admin', 'admin', 'admin@example.com', 'admin_password'),
+(NULL, 'Branch Manager', 'b_manager', 'manager_user', 'manager@example.com', 'manager_password'),
+(NULL, 'Employee User', 'employee', 'employee_user', 'employee@example.com', 'employee_password'),
+(NULL, 'Customer User 1', 'customer', 'customer_user_1', 'customer1@example.com', 'customer1_password'),
+(NULL, 'Customer User 2', 'customer', 'customer_user_2', 'customer2@example.com', 'customer2_password');
+-- Query Sep
 
 -- Insert data into Employee table
 INSERT INTO Employee (EmployeeID, BranchID, UserID, Position, IsManager) VALUES 
-('E001', 'B001', 'U002', 'Branch_Manager', TRUE),
-('E002', 'B002', 'U003', 'Other', FALSE);
+(NULL, 1, 2, 'Branch_Manager', TRUE),
+(NULL, 2, 3, 'Other', FALSE);
+-- Query Sep
 
 -- Insert data into Customer table
 INSERT INTO Customer (CustomerID, NIC_BR, DOB, Address, Phone, UserID, CustomerType) VALUES 
-('C001', '1234567890', '1990-01-15', '789 Oak St, Cityville, XYZ', '111-222-3333', 'U004', 'Individual'),
-('C002', '0987654321', '1985-05-20', '101 Pine St, Cityville, XYZ', '444-555-6666', 'U005', 'Organization');
+(NULL, '1234567890', '1990-01-15', '789 Oak St, Cityville, XYZ', '111-222-3333', 4, 'Individual'),
+(NULL, '0987654321', '1985-05-20', '101 Pine St, Cityville, XYZ', '444-555-6666', 5, 'Organization');
+-- Query Sep
 
 -- Insert data into SavingsPlan table
 INSERT INTO SavingsPlan (SavingsPlanType, InterestRate, MinimumBalance) VALUES 
@@ -27,34 +31,44 @@ INSERT INTO SavingsPlan (SavingsPlanType, InterestRate, MinimumBalance) VALUES
 ('Teen', 0.11, 500),
 ('Adult', 0.10, 1000),
 ('Senior', 0.13, 1000);
+-- Query Sep
 
 -- Insert data into Account table
-INSERT INTO Account (AccountNo, CustomerID, AccType, BranchID, Balance, SavingsPlanType, ParentID) VALUES 
-('A001', 'C001', 'Savings', 'B001', 500, 'Adult', NULL),
-('A002', 'C002', 'Savings', 'B001', 500, 'Children', 'C001'),
-('A003', 'C001', 'Checking', 'B002', 1000, NULL, NULL);
-
+INSERT INTO Account (AccountNo, CustomerID, BranchID, Balance, SavingsPlanType, ParentID) VALUES 
+('A000001', 1,  1, 1500, 'Adult', NULL),
+('A000002', 2,  1, 2500, 'Children', 1),
+('A000003', 1,  2, 2000, NULL, NULL);
+-- Query Sep
 
 
 -- Insert data into Transaction table
 INSERT INTO Transaction (TransactionID, FromAccNo, ToAccNo, TrnType, Amount) VALUES 
-('T001', 'A001', 'A002', 'Online', 200),
-('T002', 'A002', null, 'ATM', 50);
+(NULL, 'A000001', 'A000002', 'Online', 200),
+(NULL, 'A000002', null, 'ATM', 50);
+-- Query Sep
+
 
 -- Insert data into FixedDeposit table
 INSERT INTO FixedDeposit (FixedId, SavingsAccNo, StartingAmount, Duration, StartDate, LastDeptDate, InterestRate) VALUES 
-('F001', 'A001', 1000, 6, '2023-01-01', '2023-07-01', 5.00),
-('F002', 'A001', 2000, 12, '2023-01-01', '2024-01-01', 5.50);
+('F000001', 'A000001', 1000, 6, '2023-01-01', '2023-07-01', 5.00),
+('F000002', 'A000001', 2000, 12, '2023-01-01', '2024-01-01', 5.50);
+
+-- Query Sep
 
 -- Insert data into LoanApplication table
-INSERT INTO LoanApplication (LoanApplicationID, IsOnline, FixedId, CustomerID, BranchID, Duration, Type, CreatedBy, Amount, Status) VALUES 
-('L001', TRUE, 'F001', 'C001', 'B001', 12, 'Business', 'E001', 5000, 'Approved'),
-('L002', FALSE, NULL, 'C002', 'B002', 24, 'Personal', 'E002', 3000, 'Pending');
+INSERT INTO LoanApplication (LoanApplicationID, IsOnline, FixedId, CustomerID, BranchID, Duration, Type, CreatedBy, Amount, Status, CheckedBy) VALUES 
+(NULL, TRUE, 'F000001', 1, 1, 12, 'Business', NULL, 5000, 'Approved', NULL),
+(NULL, FALSE, NULL, 2, 2, 24, 'Personal', 2, 3000, 'Approved', 1),
+(NULL, FALSE, NULL, 2, 1, 36, 'Personal', 1, 6000, 'Pending', NULL);
+
+-- Query Sep
 
 -- Insert data into Loan table
 INSERT INTO Loan (LoanID, CustomerID, LoanApplicationID, Amount, StartDate, EndDate, Installment, Balance, IsOnline, FixedId) VALUES 
-('L001', 'C001', 'L001', 5000, '2023-01-01', '2023-12-01', 500, 2000, TRUE, 'F001'),
-('L002', 'C002', 'L002', 3000, '2023-01-01', '2025-01-01', 150, 2000, FALSE, NULL);
+(NULL, 1, 1, 5000, '2023-01-01', '2023-12-01', 500, 2000, TRUE, 'F000001'),
+(NULL, 1, 2, 3000, '2023-01-01', '2025-01-01', 150, 2000, FALSE, NULL);
+
+-- Query Sep
 
 -- Insert data into LoanInterestRate table
 INSERT INTO LoanInterestRate (Duration, Type, InterestRate) VALUES 
@@ -63,13 +77,19 @@ INSERT INTO LoanInterestRate (Duration, Type, InterestRate) VALUES
 (12, 'Personal', 0.16),
 (36, 'Personal', 0.17);
 
+-- Query Sep
+
 -- Insert data into LoanInstallment table
 INSERT INTO LoanInstallment (LoanID, PaymentDate, DueDate, Status) VALUES 
-('L001', '2023-02-01', '2023-02-01', 'Paid'),
-('L002', '2023-02-01', '2023-02-01', 'Pending');
+(1, '2023-02-01', '2023-02-01', 'Paid'),
+(2, '2023-02-01', '2023-02-01', 'Pending');
+
+-- Query Sep
 
 -- Insert data into FixedDepositInterestRate table
 INSERT INTO FixedDepositInterestRate (Duration, InterestRate) VALUES 
 (6, 0.13),
 (12, 0.14),
 (36, 0.15);
+
+-- Query Sep
--- scripts/views.sql
+++ scripts/views.sql
@@ -3,35 +3,65 @@ SELECT BranchID, EmployeeID, UserID, Position, Name, Role, Username, Email
 FROM Employee NATURAL JOIN User 
 WHERE IsManager = TRUE;
 
+-- Query Sep
+
 CREATE VIEW EmployeeView AS
 SELECT BranchID, EmployeeID, UserID, Position, Name, Role, Username, Email 
 FROM Employee NATURAL JOIN User 
 WHERE IsManager = FALSE;
+-- Query Sep
 
 CREATE VIEW CustomerView AS
 SELECT CustomerID, NIC_BR, DOB, Address, Phone, UserID, 
 CustomerType, Name, Role, Username, Email 
 FROM Customer NATURAL JOIN User;
+-- Query Sep
 
 CREATE VIEW SavingsAccountView AS
-SELECT a.AccountNo, a.CustomerID, a.AccType, a.BranchID, a.Balance, a.ParentID,
-s.SavingsPlanType, s.InterestRate, s.MinimumBalance 
+SELECT a.AccountNo, a.CustomerID, a.BranchID, a.Balance, a.ParentID,
+a.SavingsPlanType, s.InterestRate, s.MinimumBalance 
 FROM Account a LEFT JOIN SavingsPlan s 
 ON a.SavingsPlanType = s.SavingsPlanType 
-WHERE a.AccType = 'Savings';
+WHERE a.SavingsPlanType is not null;
+-- Query Sep
 
 CREATE VIEW ChildrenSavingsAccountView AS
-SELECT AccountNo, CustomerID, ParentID, AccType, 
+SELECT AccountNo, CustomerID, ParentID,
 BranchID, Balance, SavingsPlanType, InterestRate, MinimumBalance 
 FROM SavingsAccountView WHERE SavingsPlanType = 'Children';
+-- Query Sep
 
-
+DROP VIEW IF EXISTS MonthlyTransactionCountView;
+-- Query Sep
 CREATE VIEW MonthlyTransactionCountView AS
-SELECT COUNT(*)
-FROM Transaction 
-WHERE TrnType in ('Online', 'ATM') 
-AND MONTH(TimeStamp) = MONTH(CURDATE()) 
-AND YEAR(TimeStamp) = YEAR(CURDATE()) GROUP BY FromAccNo;
+SELECT a.AccountNo as AccountNo, 
+COUNT(
+t.TrnType in ('Online', 'ATM') 
+AND MONTH(t.TimeStamp) = MONTH(CURDATE()) 
+AND YEAR(t.TimeStamp) = YEAR(CURDATE()) ) AS Count
+FROM Transaction t RIGHT OUTER JOIN Account a ON t.FromAccNo = a.AccountNo 
+GROUP BY a.AccountNo ORDER BY a.AccountNo;
+-- Query Sep
 
-CREATE VIEW PendingLoanApplications AS 
+DROP VIEW IF EXISTS PendingLoanApplicationsView;
+-- Query Sep
+CREATE VIEW PendingLoanApplicationsView AS 
 SELECT * From LoanApplication WHERE Status = 'Pending';
+-- Query Sep
+
+DROP VIEW IF EXISTS PayableLoanInstallmentsView;
+-- Query Sep
+CREATE VIEW PayableLoanInstallmentsView AS 
+SELECT * From LoanInstallement WHERE Status in ('Pending', 'Overdue');
+
+-- Query Sep
+
+DROP VIEW IF EXISTS MinimumBalanceView;
+-- Query Sep
+CREATE VIEW MinimumBalanceView AS
+SELECT a.AccountNo as AccountNo, a.Balance as Balance, s.MinimumBalance as MinimumBalance, a.SavingsPlanType
+FROM Account a INNER JOIN SavingsPlan s ON a.SavingsPlanType = s.SavingsPlanType
+UNION
+SELECT AccountNo, Balance, -100000, SavingsPlanType
+FROM Account WHERE SavingsPlanType is null;
+-- Query Sep
