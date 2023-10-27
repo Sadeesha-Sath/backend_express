71d6536ff442d5a6559440840933e711550f741a
cneluhena <129386035+cneluhena@users.noreply.github.com>
2023-10-27 20:12:29 +0530

    reports model updated
    reports model updated to get both transaction and loan views

--- src/models/reports.model.js
+++ src/models/reports.model.js
@@ -5,11 +5,22 @@ const getAllBranches = async () => {
     return branches;
   };
 
-const getView = async(branchId)=>{
-    const views = await escapedQuery({
-        sql: 'CALL GetTransactionData(?)',
-        values: [branchId]
-    });
+const getView = async(branchId, reportType)=>{
+    let views;
+    if (reportType == 'transaction'){
+        views = await escapedQuery({
+            sql: 'CALL GetTransactionData(?)',
+            values: [branchId]
+        });
+    } else if (reportType == 'loan'){
+        views = await escapedQuery({
+            sql: 'CALL GetLoanInstallmentData(?)',
+            values: [branchId]
+        });
+    } else{
+        console.log('Error');
+    }
+        
     return views;
 }
 
