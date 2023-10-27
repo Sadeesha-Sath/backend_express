41f41abf1caa10ee5ed9d4cf080e75cbd6bc147e
cneluhena <129386035+cneluhena@users.noreply.github.com>
2023-10-27 20:11:35 +0530

    fundTransfer model updated
    fundTransfer model updated

--- src/models/fundTransfer.model.js
+++ src/models/fundTransfer.model.js
@@ -1,9 +1,9 @@
 const { query, escapedQuery } = require("../services/db.service.js");  //need to fix the path
 
 const getAllAccounts = async () => {
-    const accounts = await query('SELECT AccountNo from account;');
+    const accounts = await query('SELECT AccountNo from account');
     return accounts[0];
   };
 
 
-module.exports = { getAllAccounts, getView};
+module.exports = { getAllAccounts};
