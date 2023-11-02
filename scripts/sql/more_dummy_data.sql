680c567429a433465e3f9b72773c2d2813a4cc60
NiroshanS21 <niroshans.21@uom.lk>
2023-11-02 21:59:24 +0530

    updated dummy dta

--- scripts/sql/more_dummy_data.sql
+++ scripts/sql/more_dummy_data.sql
@@ -24,3 +24,126 @@ INSERT INTO User (UserID, Name, Role, Username, Email, Password) VALUES
  (NULL, 'Sam', 'customer', 'sam111', 'samr@user.abc.com', '$2b$10$AYFIGCzJoQzvg1WG8VJc3ebuQ5JzCjsYOXVMknyT60HyJIcrQ87t6'),
  (NULL, 'David furtitures', 'customer', 'david425', 'david99@user.abc.com', '$2b$10$kKjRrx3xLVlTCvsR76hlseMFyx2wfm28b5ciwruTM.wyA2LhMW/2a'),
  (NULL, 'Akila', 'customer', 'akila404', 'sakila@user.abc.com', '$2b$10$NCLKzUw93EqPNsmOiun9IOnWpNGIDNksjo8VHT.GJZonJ6EwXkTba');
+
+-- Insert  extra data into Branch table
+INSERT INTO Branch (BranchID, BranchName, Address, Phone, Email) VALUES 
+(NULL, 'Maincity Branch', '123 Maincity St, Cityville, XYZ', '123-456-7891', 'Maincity@example.com'),
+(NULL, 'Downcity Branch', '456 down St, Cityville, XYZ', '987-654-3211', 'Downcity@example.com'),
+(NULL, 'Upcity Branch', '789 upcity St, Cityville, XYZ', '123-456-7892', 'Upcity@example.com'),
+(NULL, 'midcity Branch', '987 midcity St, Cityville, XYZ', '987-654-3212', 'midcity@example.com'),
+(NULL, 'city Branch', '321 city St, Cityville, XYZ', '123-456-7893', 'city@example.com'),
+(NULL, 'subcity Branch', '654 subcity St, Cityville, XYZ', '987-654-3213', 'subcity@example.com'),
+(NULL, 'outcity Branch', '741 outcity St, Cityville, XYZ', '123-456-7894', 'outcity@example.com'),
+(NULL, 'backcity Branch', '147 backcity St, Cityville, XYZ', '987-654-3214', 'backcity@example.com'),
+(NULL, 'Maintown Branch', '123 Maintown St, townville, XYZ', '123-456-7895', 'Maintown@example.com'),
+(NULL, 'Uptown Branch', '789 uptown St, townville, XYZ', '123-456-7896', 'Uptown@example.com'),
+(NULL, 'midtown Branch', '987 midtown St, townville, XYZ', '987-654-3215', 'midtown@example.com'),
+(NULL, 'town Branch', '321 town St, townville, XYZ', '123-456-7897', 'town@example.com'),
+(NULL, 'subtown Branch', '654 subtown St, townville, XYZ', '987-654-3216', 'subtown@example.com'),
+(NULL, 'outtown Branch', '741 outtown St, townville, XYZ', '123-456-7898', 'outtown@example.com'),
+(NULL, 'backtown Branch', '147 backtown St, townville, XYZ', '987-654-3217', 'backtown@example.com');
+
+
+
+
+-- Insert  extra data into SavingsPlan table
+INSERT INTO SavingsPlan (SavingsPlanType, InterestRate, MinimumBalance) VALUES 
+('Children', 0.11, 100),
+('Children', 0.13, 500),
+('Children', 0.15, 1000),
+('Teen', 0.11, 500),
+('Teen', 0.12, 1000),
+('Teen', 0.15, 2000),
+('Adult', 0.10, 1000),
+('Adult', 0.13, 2000),
+('Adult', 0.16, 5000),
+('Senior', 0.13, 2000),
+('Senior', 0.14, 4000),
+('Senior', 0.17, 6000);
+
+-- Insert data into Account table
+INSERT INTO Account (AccountNo, CustomerID, BranchID, Balance, SavingsPlanType, ParentID) VALUES 
+
+('A000004', 1,  1, 1500, 'Children', NULL),
+('A000005', 2,  2, 2500, 'Children', 1),
+('A000006', 3,  3, 2000, 'Children', NULL),
+('A000007', 4,  1, 1500, 'Adult', NULL),
+('A000008', 5,  2, 2500, 'Adult', 1),
+('A000009', 6,  3, 2000, 'Adult', NULL),
+('A000010', 7,  4, 1500, 'Adult', NULL),
+('A000011', 8,  1, 2500, 'Teen', 1),
+('A000012', 9,  2, 2000, 'Teen', NULL),
+('A000013', 10,  3, 1500, 'Teen', NULL),
+('A000014', 11,  4, 2500, 'Teen', 1),
+('A000015', 12,  2, 2000, NULL, NULL),
+('A000016', 13,  1, 1500, 'Senior', NULL),
+('A000017', 14,  2, 2500, 'Senior', 1),
+('A000018', 15,  3, 2000, 'Senior', NULL),
+('A000019', 16,  4, 1500, 'Senior', NULL),
+('A000020', 17,  4, 2500, 'Children', 1),
+('A000021', 18,  5, 2000, NULL, NULL);
+
+
+-- Insert data into Transaction table
+INSERT INTO Transaction (TransactionID, FromAccNo, ToAccNo, TrnType, Amount) VALUES 
+(NULL, 'A000009', 'A000002', 'Online', 500),
+(NULL, 'A000003', 'A000004', 'ATM', 500),
+(NULL, 'A000005', 'A000002', 'Online', 1000),
+(NULL, 'A000011', null, 'ATM', 1000),
+(NULL, 'A000016', 'A000020', 'Online', 1500),
+(NULL, 'A000007', null, 'ATM', 1500),
+(NULL, 'A000006', 'A000001', 'Online', 2000),
+(NULL, 'A000002', 'A000009', 'ATM', 2000)
+(NULL, 'A000013', 'A000003', 'Online', 1200),
+(NULL, 'A000021', null, 'Online', 500);
+
+-- Insert  extra data into FixedDeposit table
+
+INSERT INTO FixedDeposit (FixedId, SavingsAccNo, StartingAmount, Duration, StartDate, LastDeptDate, InterestRate) VALUES 
+
+('F000003', 'A000003', 1000, 6, '2023-01-01', '2023-07-01', 5.00),
+('F000004', 'A000004', 2000, 12, '2023-01-02', '2024-01-05', 5.50),
+('F000005', 'A000005', 1000, 6, '2023-03-01', '2023-08-01', 5.00),
+('F000006', 'A000006', 2000, 12, '2023-04-01', '2024-09-01', 5.50),
+('F000007', 'A000007', 1000, 6, '2023-01-01', '2023-07-11', 5.00),
+('F000008', 'A000008', 2000, 12, '2022-12-12', '2024-01-01', 5.50),
+('F000009', 'A000009', 1000, 6, '2023-01-01', '2023-09-22', 5.00),
+('F000010', 'A000010', 2000, 12, '2023-06-01', '2024-08-01', 5.50);
+
+
+-- Insert  extra data into LoanApplication table
+INSERT INTO LoanApplication (LoanApplicationID, IsOnline, FixedId, CustomerID, BranchID, Duration, Type, CreatedBy, Amount, Status, CheckedBy) VALUES 
+(NULL, TRUE, 'F000003', 1, 2, 12, 'Business', NULL, 15000, 'Approved', NULL),
+(NULL, FALSE, NULL, 2, 2, 24, 'Personal', 2, 3500, 'Approved', 1),
+(NULL, FALSE, NULL, 2, 1, 36, 'Personal', 1, 6000, 'Pending', NULL),
+(NULL, TRUE, 'F000004', 3, 3, 12, 'Business', NULL, 5000, 'Approved', NULL),
+(NULL, FALSE, 'F000006', 3, 2, 24, 'Personal', 2, 4000, 'Approved', 1),
+(NULL, TRUE, 'F000009', 2, 3, 36, 'Personal', 1, 9000, 'Pending', NULL);
+
+
+-- Insert  extra data into Loan table
+INSERT INTO Loan (LoanID, CustomerID, LoanApplicationID, Amount, StartDate, EndDate, Installment, Balance, IsOnline, FixedId) VALUES 
+(NULL, 7, 6, 5000, '2023-01-01', '2023-12-02', 500, 2000, TRUE, 'F000002'),
+(NULL, 2, 5, 3000, '2023-02-01', '2025-03-01', 150, 3000, FALSE, NULL),
+(NULL, 3, 4, 5000, '2023-03-01', '2023-12-05', 500, 4000, TRUE, 'F000003'),
+(NULL, 4, 3, 3000, '2023-04-01', '2025-01-01', 150, 6000, FALSE, NULL),
+(NULL, 5, 2, 5000, '2023-05-01', '2023-12-01', 500, 5000, TRUE, 'F000004'),
+(NULL, 6, 1, 3000, '2023-06-01', '2024-01-01', 150, 3000, FALSE, NULL),
+(NULL, 8, 4, 5000, '2023-06-01', '2023-07-02', 500, 2000, TRUE, 'F000005'),
+(NULL, 9, 3, 3000, '2023-02-01', '2024-05-01', 150, 2000, FALSE, NULL);
+
+-- Insert data into LoanInterestRate table
+INSERT INTO LoanInterestRate (Duration, Type, InterestRate) VALUES 
+(06, 'Business', 0.06),
+(36, 'Business', 0.12),
+(06, 'Personal', 0.11),
+(24, 'Personal', 0.15);
+
+-- Insert  extra data into LoanInstallment table
+INSERT INTO LoanInstallment (LoanID, PaymentDate, DueDate, Status) VALUES 
+(3, '2023-02-11', '2023-02-11', 'Paid'),
+(4, '2023-03-01', '2023-03-01', 'Pending'),
+(5, '2023-04-11', '2023-04-11', 'Paid'),
+(6, '2023-05-01', '2023-05-01', 'Pending');
+
+
