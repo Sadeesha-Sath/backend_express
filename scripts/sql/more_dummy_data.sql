8447f24a75a69c2fe1f707e2f3dce8b6a959a78f
Kopimenan <kopimenan@gmail.com>
2023-11-02 17:00:22 +0530

    more dummy users added

---
+++ scripts/sql/more_dummy_data.sql
@@ -0,0 +1,141 @@
+INSERT INTO User (UserID, Name, Role, Username, Email, Password) VALUES 
+(NULL, 'Admin User', 'admin', 'admin', 'admin@example.com', '$2b$10$JYcMGTZfSfpz3oD0498wtOdfaGdGsy/Py3icvh5ZVfb4Hj4fDsBO.'),
+(NULL, 'Branch Manager', 'b_manager', 'manager_user', 'manager@example.com', '$2b$10$0ACZyZps4n2ayvW7CeAqqufx9diEOqEya6gq/msR87Auwp.r/Q0s6'),
+(NULL, 'Employee User', 'employee', 'employee_user', 'employee@example.com', '$2b$10$HgWOVugy3gF6/P5vd.jJxuBSTAftEotigQ.MkGMHovsIqWg9Y1Ocm'),
+(NULL, 'Customer User 1', 'customer', 'customer_user_1', 'customer1@example.com', '$2b$10$vIIOFCWy2yU5n8locNEOiuQ7yv3xrvGeoMt5WlQEGH6ym90Tl5zM2'),
+(NULL, 'Customer User 2', 'customer', 'customer_user_2', 'customer2@example.com', '$2b$10$yPjeOyUKqHYzfWlC4LcJS.2AUHOQHBQbLkUzlUX7vJQgJqhObz2KC');
+
+
+-- Insert data into User table
+INSERT INTO User (UserID, Name, Role, Username, Email, Password) VALUES 
+ (NULL, 'Johnson', 'admin', 'johnson123', 'johnsonh56@user.abc.com', '$2b$10$5Cctq7UmalPt3otqeFRnLOdYC4Z72WIXGvrKww5AD0Za1hA8zZCaO'),
+ (NULL, 'Kumara', 'admin', 'kumara222', 'kumarakm12@user.abc.com', '$2b$10$QBwnM5vd3C0PHDPoJoBr9uCpkfB9qtF6zfcuuFL6O9BCWJZwdPlze'),
+ (NULL, 'Albert', 'b_manger', 'albert121', 'alberts@user.abc.com', '$2b$10$RNGhaoT6u39eho.CHQBg5ehlmgaC3cuhp.POLi26uS3qI6CaRkIiC'),
+ (NULL, 'Roy', 'b_manager', 'roy555', 'roylmp@user.abc.com', '$2b$10$BormtLdDJPqcA1D.f.EAjO8UIW2PIuVvOqL.r91U7LEXLC.YVlKB2'),
+ (NULL, 'Silva', 'b_manger', 'silva123', 'silvasilva@user.abc.com', '$2b$10$uj8j6CX6J8T/jvJK8rlXvu0yEvS/TxuZYwIxKSp6X3h7GXr4ioZb.'),
+ (NULL, 'Ram', 'b_manger', 'ram969', 'ramraj9@user.abc.com', '$2b$10$N4kCtfjQcwUfwt.BGPetPuz/TIf6nXMLCK4OFoxDguuyvBykf8Huu'),
+ (NULL, 'Perera', 'b_manger', 'perera698', 'pererast@user.abc.com', '$2b$10$CuughPK2242qwfJGvT5ddegcGxn/V/z7HSzN11q.KBGpA4w7vcskK'),
+ (NULL, 'Silva', 'employee', 'silva222', 'silva404@user.abc.com', '$2b$10$tohRY6LmKULm5kz05yD7l.9IOYk46tOZIuzbpygeL9U35B3/tpPAu'),
+ (NULL, 'David', 'employee', 'david555', 'davidpil@user.abc.com', '$2b$10$WZlXfvXEbFMzLNTkp.G6R.iuyKbtUfTFPrhvryOeNZzDYyJqewP2C'),
+ (NULL, 'Charlie', 'employee', 'charlie969', 'charliest@user.abc.com', '$2b$10$ReCpWTYUv02.uclUBsfHf.bvBddXvOwzw2HWdExq1zktziarz.eoe'),
+ (NULL, 'Perera', 'employee', 'perera123', 'perera96@user.abc.com', '$2b$10$iXqt2BQybg130XUBTWc.vuNO7n3Wj7k2MrQnGNf5Wv3Pu3b.sKk2q'),
+ (NULL, 'Nimal', 'employee', 'nimal555', 'nimal69@user.abc.com', '$2b$10$kiZG/hDioVacTFdSkW0FeOtbP.yHwEGFiIMLD9fxKxRsJS/wtnVUm'),
+ (NULL, 'Supun', 'employee', 'supun886', 'supunmal@user.abc.com', '$2b$10$OoRS/eWzcQiGCjb7SPjXv.xhQMNuamntQTDr8N9X4Ey2i/CdkxVYG'),
+ (NULL, 'Harsha', 'employee', 'harsha455', 'harshaf@user.abc.com', '$2b$10$4lpZmy/Ylh9O0cJxkImn5.gK/mgDh.bXDkumZ9DduXlkwz5EmRgLy'),
+ (NULL, 'Robert', 'customer', 'robert555', 'robert2001@user.abc.com', '$2b$10$2mH9W8Xiu3QIElJCodl7xOQbJPfEVsVLVL5FYVkqGO2daUVfS8CYW'),
+ (NULL, 'Khan', 'customer', 'khan223', 'ikhan@user.abc.com', '$2b$10$/N98lodF5fxSEVqV9bDOyuqsldK1ceC66A5q9qlR6hEDxhvQnG5ya'),
+ (NULL, 'Sumudu', 'customer', 'sumudu336', 'sumuduhm@user.abc.com', '$2b$10$y.pBB2uUj3vMZlVWlFs1EuNx2ShUMTkeC1w5KV9bBFRSGlosGCZIK'),
+ (NULL, 'Raju stores', 'customer', 'raju889', 'yraju9@user.abc.com', '$2b$10$66o4L8FD5hZUbtj0HWJ38ea0k9W4eqscG5YaDPq3/9xerLhFlwQOS'),
+ (NULL, 'Poojah', 'customer', 'poojah123', 'poojah001@user.abc.com', '$2b$10$P9IunkYcwOcFROW5PvRgeONfIf0KTJXu1/fvDFLW4DsHZGUxg3tLS'),
+ (NULL, 'Nissanka', 'customer', 'nissanka555', 'nissankam@user.abc.com', '$2b$10$I86hJWbpE9NdzObrmfAZT.fZH3vq90cSrW9f831Q3OzkDp/73AZiW'),
+ (NULL, 'Mendis', 'customer', 'mendis222', 'mendiskk2@user.abc.com', '$2b$10$NPSHHb4t2A7stxGgAgux.O0pb.NKK.tYXKpIkxauGVN.lpz3y6bJ6'),
+ (NULL, 'Sam', 'customer', 'sam111', 'samr@user.abc.com', '$2b$10$AYFIGCzJoQzvg1WG8VJc3ebuQ5JzCjsYOXVMknyT60HyJIcrQ87t6'),
+ (NULL, 'David furtitures', 'customer', 'david425', 'david99@user.abc.com', '$2b$10$kKjRrx3xLVlTCvsR76hlseMFyx2wfm28b5ciwruTM.wyA2LhMW/2a'),
+ (NULL, 'Akila', 'customer', 'akila404', 'sakila@user.abc.com', '$2b$10$NCLKzUw93EqPNsmOiun9IOnWpNGIDNksjo8VHT.GJZonJ6EwXkTba');
+
+
+
+[16:19, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'AAASSS56';
+
+'$2b$10$5Cctq7UmalPt3otqeFRnLOdYC4Z72WIXGvrKww5AD0Za1hA8zZCaO'
+[16:20, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'Wsdaagh#';
+
+'$2b$10$QBwnM5vd3C0PHDPoJoBr9uCpkfB9qtF6zfcuuFL6O9BCWJZwdPlze'
+[16:22, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'Wsdaagh#';
+
+'$2b$10$QBwnM5vd3C0PHDPoJoBr9uCpkfB9qtF6zfcuuFL6O9BCWJZwdPlze';
+
+
+[16:22, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'mlkjiop@';
+
+'$2b$10$RNGhaoT6u39eho.CHQBg5ehlmgaC3cuhp.POLi26uS3qI6CaRkIiC';
+[16:23, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = '123jhuiF';
+
+'$2b$10$BormtLdDJPqcA1D.f.EAjO8UIW2PIuVvOqL.r91U7LEXLC.YVlKB2';
+[16:24, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = '1212hgfD';
+
+'$2b$10$uj8j6CX6J8T/jvJK8rlXvu0yEvS/TxuZYwIxKSp6X3h7GXr4ioZb.';
+[16:24, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'QWEasdk3';
+
+'$2b$10$N4kCtfjQcwUfwt.BGPetPuz/TIf6nXMLCK4OFoxDguuyvBykf8Huu';
+
+
+[16:25, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'jkmnbhG4';
+
+'$2b$10$CuughPK2242qwfJGvT5ddegcGxn/V/z7HSzN11q.KBGpA4w7vcskK';
+[16:26, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'rgyhuji2k';
+
+'$2b$10$tohRY6LmKULm5kz05yD7l.9IOYk46tOZIuzbpygeL9U35B3/tpPAu';
+[16:27, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = '120365GH';
+
+'$2b$10$WZlXfvXEbFMzLNTkp.G6R.iuyKbtUfTFPrhvryOeNZzDYyJqewP2C';
+[16:28, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'THAyacv$';
+
+'$2b$10$ReCpWTYUv02.uclUBsfHf.bvBddXvOwzw2HWdExq1zktziarz.eoe';
+
+
+
+
+
+[16:28, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'Swayea78';
+
+'$2b$10$iXqt2BQybg130XUBTWc.vuNO7n3Wj7k2MrQnGNf5Wv3Pu3b.sKk2q';
+[16:29, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = '635JKLhs';
+
+'$2b$10$kiZG/hDioVacTFdSkW0FeOtbP.yHwEGFiIMLD9fxKxRsJS/wtnVUm';
+[16:30, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'ZXCVhgf8';
+
+'$2b$10$OoRS/eWzcQiGCjb7SPjXv.xhQMNuamntQTDr8N9X4Ey2i/CdkxVYG';
+
+
+const password = '32&&kjiE';
+
+'$2b$10$4lpZmy/Ylh9O0cJxkImn5.gK/mgDh.bXDkumZ9DduXlkwz5EmRgLy';
+
+
+
+
+[16:33, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'PQSdF456';
+
+'$2b$10$2mH9W8Xiu3QIElJCodl7xOQbJPfEVsVLVL5FYVkqGO2daUVfS8CYW';
+[16:33, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'RTX3090$';
+
+'$2b$10$/N98lodF5fxSEVqV9bDOyuqsldK1ceC66A5q9qlR6hEDxhvQnG5ya';
+
+
+const password = 'EiPi563A';
+
+'$2b$10$y.pBB2uUj3vMZlVWlFs1EuNx2ShUMTkeC1w5KV9bBFRSGlosGCZIK';
+
+
+const password = 'Pwd0002&';
+
+'$2b$10$66o4L8FD5hZUbtj0HWJ38ea0k9W4eqscG5YaDPq3/9xerLhFlwQOS';
+
+
+const password = 'Pas$wod5';
+
+'$2b$10$P9IunkYcwOcFROW5PvRgeONfIf0KTJXu1/fvDFLW4DsHZGUxg3tLS';
+
+
+const password = 'KL89621@';
+
+'$2b$10$I86hJWbpE9NdzObrmfAZT.fZH3vq90cSrW9f831Q3OzkDp/73AZiW';
+
+
+const password = 'AKPK#$46';
+
+'$2b$10$NPSHHb4t2A7stxGgAgux.O0pb.NKK.tYXKpIkxauGVN.lpz3y6bJ6';
+
+
+const password = '!heLLoW8';
+
+'$2b$10$AYFIGCzJoQzvg1WG8VJc3ebuQ5JzCjsYOXVMknyT60HyJIcrQ87t6';
+
+
+[16:45, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'SeQ456K$';
+
+'$2b$10$kKjRrx3xLVlTCvsR76hlseMFyx2wfm28b5ciwruTM.wyA2LhMW/2a';
+[16:46, 02/11/2023] Chamod Neluhena Negombo CSE 21: const password = 'WiWop630';
+
+'$2b$10$NCLKzUw93EqPNsmOiun9IOnWpNGIDNksjo8VHT.GJZonJ6EwXkTba';
